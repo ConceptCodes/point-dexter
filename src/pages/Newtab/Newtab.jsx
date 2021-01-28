0d6f2131cd7fcbc5b831a39231e55b30742b1e8c
Concept <conceptcodes@gmail.com>
2021-01-27 19:31:32 -0500

    first commit

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import './Newtab.css';
 import './Newtab.scss';
+
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
 
 const Newtab = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+      <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            Point Dexter
+          </Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row className="py-3">
+          <Col sm="7"> 
+            <Card className="shadow shadow-md">
+              <Card.Header className="bg-dark text-white font-weight-bold">Knowledge Base</Card.Header>
+              <Card.Body>
+                <Form>
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Control as="textarea" rows={3} />
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm="5">
+            <Card>
+              <Card.Body className="chat-log">
+                <div class="chat-log">
+                  <div class="chat-log__item">
+                    <h3 class="chat-log__author">Felipe <small>14:30</small></h3>
+                    <div class="chat-log__message">Yo man</div>
+                  </div>
+                  <div class="chat-log__item chat-log__item--own">
+                    <h3 class="chat-log__author">Fabrício <small>14:30</small></h3>
+                    <div class="chat-log__message">BRB</div>
+                  </div>
+                </div>
+              </Card.Body>
+              <Card.Footer>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="What do you want to know next?"
+                  aria-label="question"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup.Append>
+                  <Button variant="dark">Send</Button>
+                </InputGroup.Append>
+              </InputGroup> 
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
 
 const Popup = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+            Knowledge Base
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Hello! I'm the body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </Container>
     </div>
   );
 };
